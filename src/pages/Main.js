e06d55eb4d64af38b592259bb5068bda4699bff0
Alexandr Tcacenco <alxtca@gmail.com>
2022-01-16 19:54:11 +0100

    added -go-  sound effect

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,6 +5,8 @@ import React, {useState, useEffect} from "react"
 import useSound from 'use-sound'
 import boopSfx from '../sounds/beep-07a.mp3'
 import boopEnd from '../sounds/applause.wav'
+import boopGo from '../sounds/go.mp3'
+import boopRelax from '../sounds/relax.mp3'
 
 const Main = (props) => {
   const [working, setWorking] = useState(false)
@@ -13,10 +15,12 @@ const Main = (props) => {
   //const [workoutTemp, setWorkoutTemp] = useState({work: props.workout.work, rest: props.workout.rest, rounds: props.workout.rounds})
   const [work, setWork] = useState(props.workout.work)
   const [rest, setRest] = useState(props.workout.rest)
-  const [rounds, setRounds] = useState(1)
+  const [rounds, setRounds] = useState(1) //current round
   const [delay, setDelay] = useState(props.workout.delay)
   const [play] = useSound(boopSfx)
   const [playEnd] = useSound(boopEnd)
+  const [playGo] = useSound(boopGo)
+  const [playRelax] = useSound(boopRelax)
 
   useEffect(()=>{
 
@@ -24,22 +28,26 @@ const Main = (props) => {
     {
       if (delay  > 0) {
         setTimeout(() => {
-          if (delay < 4) {  play()   }
+          if (delay === 4) {  playGo()   }
           setDelay(delay-1)
         }, 1000)
       }
       else {
         if (work > 0) { //countdown work
           setTimeout(() => {
-            if (work < 4) {  play()   }
+            if (work < 4 && work > 1) {  play()   }
             setWork(work-1)
+            if (work === 1) {  playRelax()   }
+            setWork(work-1)
+
           }, 1000)
         }
   
         if (work === 0) { //countdown rest
           if (rest > 0){
             setTimeout(() => {
-              if (rest < 4) {  play()   }
+              if ((rest === 4) && (rounds < props.workout.rounds)) { //second condition to avoid 'go' sound after last work set
+                  playGo()   }
               setRest(rest-1)
             }, 1000)
           }
@@ -69,7 +77,9 @@ const Main = (props) => {
       setRounds(1)
       setDelay(props.workout.delay)
     }
-  }, [work, rest, rounds, working, delay, props.workout.work, props.workout.rest, props.workout.rounds, props.workout.delay, play, playEnd])
+  }, [work, rest, rounds, working, delay,
+     props.workout.work, props.workout.rest, props.workout.rounds, props.workout.delay,
+      play, playEnd, playGo, playRelax])
 
   const switchWork = () => {
     setFanfara(false)
